816db7eb9796ce1d8e5e0771bed1f2fcd4a40714
Renqian Luo <lrqrichard@gmail.com>
2019-01-08 23:15:29 +0800

    ws train search

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -228,7 +228,7 @@ def main():
         sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),
         pin_memory=True, num_workers=16)
     valid_queue = torch.utils.data.DataLoader(
-        train_data, batch_size=args.eval_batch_size,
+        train_data, batch_size=args.child_eval_batch_size,
         sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
         pin_memory=True, num_workers=16)
 
