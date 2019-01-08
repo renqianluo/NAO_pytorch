e6b0bf183c55dac2a1985c271d1bd8cf894c2ce4
Renqian Luo <lrqrichard@gmail.com>
2019-01-09 03:37:09 +0800

    ws train search

--- cnn/test_controller.py
+++ cnn/test_controller.py
@@ -154,7 +154,8 @@ def main():
     nao_optimizer = torch.optim.Adam(nao.parameters(), lr=args.controller_lr, weight_decay=args.controller_l2_reg)
     for nao_epoch in range(1, args.controller_epochs + 1):
         nao_loss, nao_mse, nao_ce = nao_train(nao_train_queue, nao, nao_optimizer)
-        logging.info("epoch %04d train loss %.2f mse %.2f ce %.2f", nao_epoch, nao_loss, nao_mse, nao_ce)
+        if nao_epoch % 10 == 0:
+            logging.info("epoch %04d train loss %.2f mse %.2f ce %.2f", nao_epoch, nao_loss, nao_mse, nao_ce)
         if nao_epoch % 100 == 0:
             pa, hs = nao_valid(nao_valid_queue, nao)
             logging.info("Evaluation on training data\n")
