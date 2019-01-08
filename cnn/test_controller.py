8d42af5d7ae062862864be5602058dfba776997b
Renqian Luo <lrqrichard@gmail.com>
2019-01-09 02:47:08 +0800

    ws train search

--- cnn/test_controller.py
+++ cnn/test_controller.py
@@ -57,11 +57,13 @@ def nao_train(train_queue, model, optimizer):
     mse = utils.AvgrageMeter()
     nll = utils.AvgrageMeter()
     model.train()
-    for step, (encoder_input, encoder_target, decoder_input, decoder_target) in enumerate(train_queue):
+    for step, (encoder_input, encoder_target) in enumerate(train_queue):
+        n = encoder_input.size(0)
+        decoder_input = torch.cat([torch.LongTensor([0 for i in range(n)]), encoder_input[:, :-1]], dim=1)
         encoder_input = Variable(encoder_input).cuda()
         encoder_target = Variable(encoder_target).cuda(async=True)
         decoder_input = Variable(decoder_input).cuda()
-        decoder_target = Variable(decoder_target).cuda(async=True)
+        decoder_target = Variable(encoder_input).cuda(async=True)
         
         optimizer.zero_grad()
         predict_value, log_prob, arch = model(encoder_input, decoder_input)
@@ -84,10 +86,10 @@ def nao_valid(queue, model):
     pa = utils.AvgrageMeter()
     hs = utils.AvgrageMeter()
     model.eval()
-    for step, (encoder_input, encoder_target, decoder_target) in enumerate(queue):
+    for step, (encoder_input, encoder_target) in enumerate(queue):
         encoder_input = Variable(encoder_input, volatile=True).cuda()
         encoder_target = Variable(encoder_target, volatile=True).cuda(async=True)
-        decoder_target = Variable(decoder_target, volatile=True).cuda(async=True)
+        decoder_target = Variable(encoder_input, volatile=True).cuda(async=True)
         predict_value, logits, arch = model(encoder_input)
         n = encoder_input.size(0)
         pairwise_acc = utils.pairwise_accuracy(encoder_target.data[0].squeeze().tolist(),
@@ -101,7 +103,7 @@ def nao_valid(queue, model):
 def nao_infer(queue, model, step):
     new_arch_list = []
     model.eval()
-    for i, (encoder_input) in enumerate(queue):
+    for i, encoder_input in enumerate(queue):
         encoder_input = Variable(encoder_input).cuda()
         model.zero_grad()
         new_arch = model.generate_new_arch(encoder_input, step)
