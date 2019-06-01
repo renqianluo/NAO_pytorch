b8339eaae26fc298ec0d76e26814053ebbc667b6
Renqian Luo <renqianluo@gmail.com>
2019-06-01 23:31:29 +0800

    bugs fix

--- train_imagenet.py
+++ train_imagenet.py
@@ -55,18 +55,18 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
 
 class CrossEntropyLabelSmooth(nn.Module):
 
-  def __init__(self, num_classes, epsilon):
-    super(CrossEntropyLabelSmooth, self).__init__()
-    self.num_classes = num_classes
-    self.epsilon = epsilon
-    self.logsoftmax = nn.LogSoftmax(dim=1)
+    def __init__(self, num_classes, epsilon):
+        super(CrossEntropyLabelSmooth, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.logsoftmax = nn.LogSoftmax(dim=1)
 
-  def forward(self, inputs, targets):
-    log_probs = self.logsoftmax(inputs)
-    targets = torch.zeros_like(log_probs).scatter_(1, targets.unsqueeze(1), 1)
-    targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
-    loss = (-targets * log_probs).mean(0).sum()
-    return loss
+    def forward(self, inputs, targets):
+        log_probs = self.logsoftmax(inputs)
+        targets = torch.zeros_like(log_probs).scatter_(1, targets.unsqueeze(1), 1)
+        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+        loss = (-targets * log_probs).mean(0).sum()
+        return loss
 
 
 def train(train_queue, model, optimizer, global_step, criterion):
