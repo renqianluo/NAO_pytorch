60ff1b0990f811e76bc8c66709f216e6f005b46e
Renqian Luo <renqianluo@gmail.com>
2019-06-03 23:39:53 +0800

    warnings

--- model.py
+++ model.py
@@ -272,7 +272,7 @@ class NASNetworkCIFAR(nn.Module):
     def init_parameters(self):
         for w in self.parameters():
             if w.data.dim() == 4:
-                nn.init.kaiming_normal(w.data)
+                nn.init.kaiming_normal_(w.data)
     
     def forward(self, input, step=None):
         aux_logits = None
