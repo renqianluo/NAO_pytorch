50a1e71ce2d1896d514dc193711700333d22ddb0
Renqian Luo <renqianluo@gmail.com>
2019-06-12 11:14:58 +0800

    add scripts

---
+++ train_imagenet.sh
@@ -0,0 +1,20 @@
+nvidia-smi
+MODEL=imagenet
+OUTPUT_DIR=exp/$MODEL
+DATA_DIR=imagenet/raw-data
+
+mkdir -p $OUTPUT_DIR
+
+fixed_arc="0 7 1 15 2 8 1 7 0 6 1 7 0 8 4 7 1 5 0 7 0 7 1 13 0 6 0 14 0 9 1 10 0 14 2 6 1 11 0 7"
+
+python train_imagenet.py \
+  --data=$DATA_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --batch_size=128 \
+  --epochs=250 \
+  --arch="$fixed_arc" \
+  --channels=42 \
+  --use_aux_head \
+  --lr=0.1 \
+  --keep_prob=1.0 \
+  --drop_path_keep_prob=1.0 | tee -a $OUTPUT_DIR/train.log
