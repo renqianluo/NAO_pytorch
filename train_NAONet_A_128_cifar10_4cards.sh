6ef2c7f08263586c3063a669fd87d1eb8796c551
Renqian Luo <renqianluo@gmail.com>
2019-06-21 11:07:43 +0800

    typo

--- train_NAONet_A_128_cifar10_4cards.sh
+++ train_NAONet_A_128_cifar10_4cards.sh
@@ -7,7 +7,7 @@ mkdir -p $OUTPUT_DIR
 
 fixed_arc="0 7 1 15 2 8 1 7 0 6 1 7 0 8 4 7 1 5 0 7 0 7 1 13 0 6 0 14 0 9 1 10 0 14 2 6 1 11 0 7"
 
-python train_imagenet.py \
+python train_cifar.py \
   --data=$DATA_DIR \
   --output_dir=$OUTPUT_DIR \
   --batch_size=128 \
