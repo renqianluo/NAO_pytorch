c2a44d65ebaafb93fe659ac2e8ebd688a1cbf58c
Renqian Luo <renqianluo@gmail.com>
2019-09-09 19:10:47 +0800

    scripts

---
+++ NAO_V1/train_NAONet_A_36_cifar10.sh
@@ -0,0 +1,16 @@
+# Train the architecture discovered by NAO, with channel size of 36, noted as NAONet-A-36
+nvidia-smi
+MODEL=NAONet_A_36_cifar10
+OUTPUT_DIR=exp/$MODEL
+DATA_DIR=data
+
+mkdir -p $OUTPUT_DIR
+
+fixed_arc="0 7 1 15 2 8 1 7 0 6 1 7 0 8 4 7 1 5 0 7 0 7 1 13 0 6 0 14 0 9 1 10 0 14 2 6 1 11 0 7"
+
+python train_cifar.py \
+  --data=$DATA_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --arch="$fixed_arc" \
+  --channels=36 \
+  --cutout_size=16 | tee -a $OUTPUT_DIR/train.log
---
+++ NAO_V1/train_NAONet_A_imagenet_4cards.sh
@@ -0,0 +1,19 @@
+# Train the architecture discovered by NAO, noted as NAONet-A, on ImageNet, with 4 cards
+nvidia-smi
+MODEL=NAONet_A_imagenet
+OUTPUT_DIR=exp/$MODEL
+DATA_DIR=imagenet/raw-data
+
+mkdir -p $OUTPUT_DIR
+
+fixed_arc="0 7 1 15 2 8 1 7 0 6 1 7 0 8 4 7 1 5 0 7 0 7 1 13 0 6 0 14 0 9 1 10 0 14 2 6 1 11 0 7"
+
+python train_imagenet.py \
+  --data=$DATA_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --batch_size=512 \
+  --epochs=250 \
+  --arch="$fixed_arc" \
+  --channels=42 \
+  --use_aux_head \
+  --lr=0.4 | tee -a $OUTPUT_DIR/train.log
