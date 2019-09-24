af1167eded0fce96db55de79905e4a73959f373a
Renqian Luo <renqianluo@gmail.com>
2019-09-24 16:24:51 +0800

    bug fix

--- NAO_V1/train_NAONet_A_36_cifar10.sh
+++ NAO_V1/train_NAONet_A_36_cifar10.sh
@@ -6,11 +6,12 @@ DATA_DIR=data
 
 mkdir -p $OUTPUT_DIR
 
-fixed_arc="0 7 1 15 2 8 1 7 0 6 1 7 0 8 4 7 1 5 0 7 0 7 1 13 0 6 0 14 0 9 1 10 0 14 2 6 1 11 0 7"
+fixed_arc="0 2 1 10 2 3 1 2 0 1 1 2 0 3 4 2 1 0 0 2 0 2 1 8 0 1 0 9 0 4 1 5 0 9 2 1 1 6 0 2"
 
 python train_cifar.py \
   --data=$DATA_DIR \
   --output_dir=$OUTPUT_DIR \
+  --search_space=large \
   --arch="$fixed_arc" \
   --use_aux_head \
   --cutout_size=16 | tee -a $OUTPUT_DIR/train.log
